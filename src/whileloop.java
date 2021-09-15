ab59ca45a7a409f0f26eb598babd956f255b307b
ansfridpereira <89774958+ansfridpereira@users.noreply.github.com>
2021-09-15 10:45:12 +0530

    Program to print numbers from 1 to 100

---
+++ src/whileloop.java
@@ -0,0 +1,11 @@
+/* printing 100 numbers*/
+public class whileloop {
+    public static void main(String[] args)
+    {
+        int x;
+        for(x=0; x<100; x=x+1)
+        {
+            System.out.println("x="+x);
+        }
+    }
+}
